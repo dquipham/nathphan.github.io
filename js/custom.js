23a8a521569215fba91178939e1b96cdf9b7d09b
Nathan Phan <nathanphan@asa-vpn-80-182.ucsc.edu>
2017-07-20 16:24:46 -0700

    first

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(function(){
+    jQuery(document).ready(function() {
+    $('#home').backstretch([
+       "imgs/home-bg-slideshow1.jpg", 
+       "imgs/home-bg-slideshow2.jpg",
+        ],  {duration: 2000, fade: 750});
+    });
+  })
